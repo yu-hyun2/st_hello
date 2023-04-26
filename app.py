00a72a67b45678aeb81fec668681c3ec8dd4eaaf
U HYUN <121507181+yu-hyun2@users.noreply.github.com>
2023-04-26 11:12:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,12 @@ import numpy as np
 
 st.title('Uber pickups in NYC')
 
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
             'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
